95f63e2084afb55066f8d9ba13ee227a693c9b0d
Marwa <marwa.niaaj@gmail.com>
2024-02-04 13:28:17 +0400

    Add `CustomAlert`.

---
+++ app/src/main/java/com/example/customalert/CustomAlert.kt
@@ -0,0 +1,228 @@
+package com.example.customalert
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.example.customalert.ui.theme.AlertCyan
+
+
+@Composable
+fun <T> CustomAlert(
+    title: String, message: String,
+    actionText: String,
+    presenting: T?,
+    showAlert: MutableState<Boolean>,
+    actionWithValue: ((T) -> Unit)?,
+    action: (() -> Unit)?,
+) {
+    val animationState = remember { MutableTransitionState(false) }
+
+    /// Run only once
+    LaunchedEffect(Unit) {
+        animationState.targetState = showAlert.value
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        AnimatedVisibility(
+            animationState,
+            enter = slideInHorizontally(
+                animationSpec = tween(200)
+            ),
+            exit = slideOutHorizontally(
+                animationSpec = tween(200),
+                targetOffsetX = { fullWidth -> fullWidth }
+            )
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp),
+                shape = RoundedCornerShape(35.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.secondary
+                )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        title,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = AlertCyan,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.headlineMedium,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Text(
+                        message,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontSize = 19.sp,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+
+                    /// Buttons
+                    Row(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Button(
+                            onClick = {
+                                animationState.targetState = false
+                            },
+                            modifier = Modifier
+                                .background(
+                                    MaterialTheme.colorScheme.surface,
+                                    shape = RoundedCornerShape(35.dp)
+                                )
+                                .height(55.dp)
+                                .weight(1f),
+                            shape = RoundedCornerShape(0.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.White,
+                                disabledContainerColor = Color.Transparent,
+                            ),
+                        ) {
+                            Text(
+                                text = "Cancel",
+                                modifier = Modifier,
+                                color = AlertCyan,
+                                fontWeight = FontWeight.SemiBold,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        }
+                        Button(
+                            onClick = {
+                                animationState.targetState = false
+
+                                if (actionWithValue != null) {
+                                    presenting?.let {
+                                        actionWithValue(presenting)
+                                    }
+                                } else if (action != null) {
+                                    action()
+                                }
+                            },
+                            modifier = Modifier
+                                .background(
+                                    AlertCyan,
+                                    shape = RoundedCornerShape(35.dp)
+                                )
+                                .height(55.dp)
+                                .weight(1f),
+                            shape = RoundedCornerShape(0.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.White,
+                                disabledContainerColor = Color.Transparent,
+                            ),
+                        ) {
+                            Text(
+                                text = actionText,
+                                modifier = Modifier,
+                                fontWeight = FontWeight.SemiBold,
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        /// Current state changes after animation finishes
+        LaunchedEffect(animationState.currentState) {
+            /// Animation is completed when current state is the same as target state.
+            /// And currentState is false
+            if (!animationState.currentState && animationState.targetState == animationState.currentState) {
+                showAlert.value = false
+            }
+        }
+    }
+}
+
+@Composable
+fun <T> CustomAlertDialog(
+    title: String,
+    message: String,
+    actionText: String,
+    presenting: T?,
+    showAlert: MutableState<Boolean>,
+    action: (T) -> Unit
+) {
+    Dialog(
+        onDismissRequest = { showAlert.value = false },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        CustomAlert(
+            title, message, actionText, presenting, showAlert,
+            actionWithValue = action, action = null
+        )
+    }
+}
+
+@Composable
+fun CustomAlertDialog(
+    title: String,
+    message: String,
+    actionText: String,
+    showAlert: MutableState<Boolean>,
+    action: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = { showAlert.value = false },
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        CustomAlert(
+            title, message, actionText, null, showAlert,
+            actionWithValue = null, action = action
+        )
+    }
+}
--- app/src/main/java/com/example/customalert/MainActivity.kt
+++ app/src/main/java/com/example/customalert/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -60,7 +62,7 @@ fun MainScreen() {
         Book(title = "Limitless", author = "Jim Kwik")
     )
     val books by remember { mutableStateOf(booksList) }
-    val randomBook = remember { mutableStateOf<Book?>(null) }
+    var randomBook by remember { mutableStateOf<Book?>(null) }
     val selectedBook = remember { mutableStateOf<Book?>(null) }
 
     val showBookAlert = remember { mutableStateOf(false) }
@@ -74,11 +76,13 @@ fun MainScreen() {
                     containerColor = MaterialTheme.colorScheme.background,
                 ),
                 actions = {
-                    Icon(
-                        imageVector = Icons.Filled.AccountBox,
-                        contentDescription = "",
-                        tint = AlertCyan
-                    )
+                    IconButton(onClick = { showCreditsAlert.value = true }) {
+                        Icon(
+                            imageVector = Icons.Filled.AccountBox,
+                            contentDescription = "",
+                            tint = AlertCyan
+                        )
+                    }
                 }
             )
         },
@@ -95,7 +99,8 @@ fun MainScreen() {
         ) {
             ElevatedButton(
                 onClick = {
-                    randomBook.value = books.random()
+                    randomBook = books.random()
+                    showBookAlert.value = true
                 }
             ) {
                 Text(text = "Select Random Book", color = AlertCyan)
@@ -121,18 +126,35 @@ fun MainScreen() {
         }
 
         if (showBookAlert.value) {
-            // TODO: Show Book Alert
+            // Show Book Alert
+            CustomAlertDialog(
+                "Random Book",
+                "You have selected ${randomBook?.title} by ${randomBook?.author}",
+                "Yes, Done",
+                randomBook,
+                showBookAlert,
+            ) {
+                selectedBook.value = it
+            }
         }
 
         if (showCreditsAlert.value) {
-            // TODO: Show Credits Alert
+            // Show Credits Alert
+            CustomAlertDialog(
+                "Credits",
+                "This custom alert tutorial was implemented by Marwa",
+                "OK",
+                showCreditsAlert
+            ) {
+                // Action
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AlertPreview() {
     CustomAlertTheme {
         MainScreen()
     }
